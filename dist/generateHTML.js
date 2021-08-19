9ed18d0c0c855d88317fff229df19b0a0f676920
Alena Rehberger <alenarehberger92@gmail.com>
2021-08-19 12:34:54 -0500

    Working on index.html

---
+++ dist/generateHTML.js
@@ -0,0 +1,13 @@
+
+
+
+
+// TODO: Create a function to generate HTML for README
+function generateHTML(data) {
+    return `# ${data.title}
+
+  `;
+    
+  }
+  
+  module.exports = generateHTML;
--- index.js
+++ index.js
@@ -3,105 +3,147 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
-const generateHTML = require ('./dist:/generateHTML');
-// TODO: Create an array of questions for user input
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+// const generateHTML = require ('./dist/generateHTML');
+
+var team = [];
+
+// an array of questions for user input
 const questions = [
     {
         type: 'input',
         name: 'managerName',
-        message: `What is the team manager's name?`
+        message: `What is the team manager's name?`,
+        // validateName: validateName = string => {
+        //     return string !== '' || `Please put in the team manager's name to continue`
+        // }
     },
     {
         type: 'input',
         name: 'managerId',
-        message: `What is the team manager's ID?`
+        message: `What is the team manager's ID?`,
+        // validateId:
     },
     {
         type: 'input',
         name: 'managerEmail',
-        message: `What is the team manager's email?`
+        message: `What is the team manager's email?`,
+        // validateEmail
     },
     {
         type: 'input',
         name: 'managerOfficeNumber',
-        message: `What is the team manager's office number?`
+        message: `What is the team manager's office number?`,
+        // validateOffice
     },
+
+
     {
         type: 'list',
         name: 'teamMember',
         message: 'What type of team member would you like to add?',
         choices: ['Engineer', 'Intern', `I don't want to add any more team members`]
     },
-    {
-        type: 'input',
-        name: 'engineerName',
-        message: `What is your engineer's name?`
-    },
-    {
-        type: 'input',
-        name: 'engineerId',
-        message: `What is your engineer's ID?`
-    },
-    {
-        type: 'input',
-        name: 'engineerEmail',
-        message: `What is your engineer's email?`
-    },
-    {
-        type: 'input',
-        name: 'engineerGithub',
-        message: `What is your engineer's GitHub username?`
-    },
-    {
-        type: 'list',
-        name: 'teamMember',
-        message: 'What type of team member would you like to add?',
-        choices: ['Engineer', 'Intern', `I don't want to add any more team members`]
-    },
-    {
-        type: 'input',
-        name: 'internName',
-        message: `What is your intern's name?`
-    },
-    {
-        type: 'input',
-        name: 'internId',
-        message: `What is your intern's ID?`
-    },
-    {
-        type: 'input',
-        name: 'internEmail',
-        message: `What is your intern's email?`
-    },
-    {
-        type: 'input',
-        name: 'internSchool',
-        message: `What is your intern's school?`
-    },
-    {
-        type: 'list',
-        name: 'teamMember',
-        message: 'What type of team member would you like to add?',
-        choices: ['Engineer', 'Intern', `I don't want to add any more team members`]
-    }
+
+
+    // {
+    //     type: 'input',
+    //     name: 'engineerName',
+    //     message: `What is your engineer's name?`
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'engineerId',
+    //     message: `What is your engineer's ID?`
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'engineerEmail',
+    //     message: `What is your engineer's email?`
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'engineerGithub',
+    //     message: `What is your engineer's GitHub username?`
+    // },
+    // 
+    // {
+    //     type: 'input',
+    //     name: 'internName',
+    //     message: `What is your intern's name?`
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'internId',
+    //     message: `What is your intern's ID?`
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'internEmail',
+    //     message: `What is your intern's email?`
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'internSchool',
+    //     message: `What is your intern's school?`
+    // },
+    // {
+    //     type: 'list',
+    //     name: 'teamMember',
+    //     message: 'What type of team member would you like to add?',
+    //     choices: ['Engineer', 'Intern', `I don't want to add any more team members`]
+    // }
     
 ];
 
-// TODO: Create a function to write HTML file
+// function to write HTML file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
         err ? console.log(err) : console.log("Success! Your HTML file has been generated");
     })
 }
 
-// TODO: Create a function to initialize app
+// function to initialize app
 function init() {
     inquirer.prompt(questions)
-        .then(function(data) {
-            writeToFile('team.html', generateHTML(data));
+        .then((data) => {
+            let manager = new Manager(data.name, data.id, data.email, data.office);
+            team.push(manager);
+            // writeToFile('index.html', generateHTML(data));
             // console.log(data)
+            addNextMember();
         })
 }
+
+const addNextMember = () => {
+    inquirer
+        .prompt ([
+            {
+                type: 'list',
+                name: 'teamMember',
+                message: 'What type of team member would you like to add?',
+                choices: ['Engineer', 'Intern', `I don't want to add any more team members`]
+            },
+        ])
+        .then((data) => {
+            if (data.employee === 'Engineer' ) {
+                addEngineer ();
+            } 
+            if (data.employee === 'Intern') {
+                addIntern();
+            }
+            else {
+                createTeam();
+            }
+        });
+};
+
+//create a function addEngineer() with prompt;
+//create a function addIntern() with prompt;
+//create a function createTeam() with mock??;
 
 // Function call to initialize app
 init();
