7f4461765252ef94dec1506db953b6ae60a73eb2
Alena Rehberger <alenarehberger92@gmail.com>
2021-08-20 18:16:07 -0500

    Manager tests have passed

--- _tests_:/Manager.test.js
+++ _tests_:/Manager.test.js
@@ -1,13 +1,20 @@
-// const Manager = require('../lib/manager');
+const Manager = require('../lib/manager');
 
-// describe("Manager", () => {
-//     //getRole() should return "Manager"
-//     it("getRole() should return 'Manager'", () => {
-//        const testValue = new Manager(); 
-//        expect(testValue.getRole()).toEqual("Manager");
-//     });
+describe("Manager", () => {
+    //getRole() should return "Manager"
+    it("getRole() should return 'Manager'", () => {
+       const testValue = new Manager(); 
+       expect(testValue.getRole()).toEqual("Manager");
+    });
 
-//     //test for the office number via getOffice()
-
-//     //can get office number via Constructor
-// });
+    //test for the office number via getOfficeNumber()
+    it("test for the office number via getOfficeNumber()", () => {
+        const testValue = new Manager("Jon", 555, "test@test.com", 777); 
+        expect(testValue.getOfficeNumber()).toBe(777);
+    });
+    //can get office number via Constructor
+    it("can get office number via Constructor", () => {
+        const testValue = new Manager("Jon", 555, "test@test.com", 777); 
+        expect(testValue.officeNumber).toBe(777);
+    });
+});
