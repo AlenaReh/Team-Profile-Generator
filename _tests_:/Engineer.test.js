d81f538823b242354ef1d22c9cea655297edbd85
Alena Rehberger <alenarehberger92@gmail.com>
2021-08-20 18:10:14 -0500

    Engineer tests have passes

--- _tests_:/Engineer.test.js
+++ _tests_:/Engineer.test.js
@@ -1,16 +1,20 @@
-// const Engineer = require('../lib/engineer');
-   
+const Engineer = require('../lib/engineer');
     
-// describe("Engineer", () => {
-//         //getRole() should return "Engineer"
-//     it("getRole() should return 'Engineer'", () => {
-//         const testValue = new Engineer(); 
-//         expect(testValue.getRole()).toEqual("Engineer");
-//     });
-//     //test for the GitHub username via getGitHub()
-    
-//     //can get GitHub via Constructor via getSchool();
-
-
-// });
+describe("Engineer", () => {
+        //getRole() should return "Engineer"
+    it("getRole() should return 'Engineer'", () => {
+        const testValue = new Engineer(); 
+        expect(testValue.getRole()).toEqual("Engineer");
+    });
+    //test for the GitHub username via getGitHub()
+    it("test for the school name via getGitHub()", () => {
+        const testValue = new Engineer("Aria", 333, "aria@test.com", "https://github.com/AriaStark"); 
+        expect(testValue.getGithub()).toBe("https://github.com/AriaStark");
+    });
+    //test for the setting of the GitHub username via getGitHub()
+    it("test for the setting of the GitHub username via getGitHub()", () => {
+        const testValue = new Engineer("Aria", 333, "aria@test.com", "https://github.com/AriaStark"); 
+        expect(testValue.githubUsername).toBe("https://github.com/AriaStark");
+    });
+});
     
